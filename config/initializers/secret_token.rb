43538e70b1564ab5e2e11b1b7d9131302164a770
Wanderley Junior <wwwj@outlook.com>
2013-08-06 02:44:46 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sisdun::Application.config.secret_key_base = 'a6c20ec09b2e49fdd03bc6afb538d6a319acdadac518fe6210507d60f5ca668bfc3dc3fb1aa985cfe1ef22fce0e305e4b91e9e803173dd4f37d4fe7267c51817'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sisdun::Application.config.session_store :cookie_store, key: '_sisdun_session'
