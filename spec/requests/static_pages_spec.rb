464832e10653f9debbe03e2018b0de33522777eb
Wanderley Junior <wwwj@outlook.com>
2013-08-07 12:50:43 -0300

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,76 +2,40 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
-
-    it "should have the h1 'Bem-vindo ao SISDUN'" do
-      visit root_path
-      expect(page).to have_content('Sisdun')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Sisdun")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
-  end
-
-  describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("| Help")
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the h1 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("| About Us")
-    end
-  end
-end
-
-require 'spec_helper'
-
-describe "Static pages" do
-
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sisdun') }
+    it { should have_selector('h1', text: 'Bem-vindo ao SISDUN') }
     it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it { should_not have_title '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
+    it { should have_selector('h1', text: 'About Us') }
     it { should have_title(full_title('About Us')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_title full_title('About Us')
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    #click_link "Sign up"
+    #expect(page).to have_title(full_title('Sign up'))
+    click_link "sisdun"
+    expect(page).to have_selector 'h1', text: 'Bem-vindo ao SISDUN'
   end
 end
