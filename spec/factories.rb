47d5147958a92c23c4d05404e8f9d8c391bf403c
Wanderley Junior <wwwj@outlook.com>
2013-08-08 03:13:47 -0300

    Finish user signup add SSL in production

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Wanderley Junior"
+    email    "wanderley@example.com"
+    password "secret"
+    password_confirmation "secret"
+  end
+end
