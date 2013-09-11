0d2a4f73832c3a044146d017f4ead93e37edfe6c
Wanderley Junior <wwwj@outlook.com>
2013-09-11 10:32:36 -0300

    finalizado layout e jquery, faltando validação cpf

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,15 @@
 class Customer < ActiveRecord::Base
+
+  validates :first_name,  presence: true,
+                          length: { maximum: 30 }
+  validates :last_name, presence:true,
+                        length: { maximum: 70 }
+  validates :gender,  presence: true,
+                      inclusion: { in: %w( male female ) }
+  VALID_CPF_REGEX = /\d{3}\.\d{3}\.\d{3}-\d{2}/
+  validates :cpf, presence: true,
+                  uniqueness: true,
+                  format: { with: VALID_CPF_REGEX }
+  validates :birth_date,  presence: true
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,13 +3,81 @@ require 'spec_helper'
 describe Customer do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @customer = Customer.new(first_name: "Example", last_name: "User", gender: "male", cpf: "22406513858", birth_date: "1995-07-12")
   end
 
   subject { @customer }
 
-  it { shold respond_to(:name) }
-  it { shold respond_to(:email) }
-  it { shold respond_to(:cpf) }
-  it { shold respond_to(:phone) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:gender) }
+  it { should respond_to(:cpf) }
+  it { should respond_to(:birth_date) }
+
+  it { should be_valid }
+  
+  describe "when first name is not present" do
+    before { @customer.first_name = " " }
+    it { should_not be_valid}
+  end
+
+  describe "when last name is not present" do
+    before { @customer.last_name = " " }
+    it { should_not be_valid}
+  end
+
+  describe "when gender is not present" do
+    before { @customer.gender = " " }
+    it { should_not be_valid}
+  end
+
+  describe "when cpf is not present" do
+    before { @customer.cpf = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when birth date is not present" do
+    before { @customer.birth_date = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when first name is too long" do
+    before { @customer.first_name = "a" * 31 }
+    it { should_not be_valid }
+  end
+
+  describe "when last name is too long" do
+    before { @customer.last_name = "a" * 71 }
+    it { should_not be_valid }
+  end
+
+  describe "when gender name is valid" do
+    it "should be valid" do
+      gender = %w[male female]
+      gender.each do |valid_gender|
+        @customer.gender = valid_gender
+        expect(@customer).to be_valid
+      end
+    end
+  end
+
+  describe "when cpf is too long" do
+    before { @customer.cpf = "1" * 12 }
+    it { should_not be_valid }
+  end
+
+  describe "when cpf is too short" do
+    before { @customer.cpf = "1" * 10 }
+    it { should_not be_valid }
+  end
+
+  describe "when cpf is already taken" do
+    before do
+      customer_with_same_cpf = @customer.dup
+      customer_with_same_cpf.cpf = @customer.cpf
+      customer_with_same_cpf.save
+    end
+
+    it { should_not be_valid }
+  end
+end
