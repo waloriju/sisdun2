a3dd2b1bb3630c056141b8f3e3251006cfb0344c
Wanderley Junior <waloriju@gmail.com>
2014-12-08 14:56:02 -0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 #   users = User.create(name: 'Wanderley Junior', email: 'admin@email.com', password: 'foobar', password_confirmation: 'foobar', admin: true)
 #   users = User.create(name: 'user', email: 'user@email.com', password: 'foobar', password_confirmation: 'foobar', admin: false)
-#   companies = Company.create(company_name: 'Wanderley Lopes Ribeiro Junior 05092919965', trading_name: 'Wanderley Junior', cnpj: '76.247.345/0001-06')
+#   companies = Company.create(company_name: 'Wanderley Lopes Ribeiro Junior 58899918414', trading_name: 'Wanderley Junior', cnpj: '25.637.541/0001-56')
 #   addresses = Address.create(line1: 'Rua Prefeito Carlito S Vilella', line2: 'Casa', number: '15', postal_code: '87430-000', city_id: '3160')
